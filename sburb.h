871c8071c50451c0b57dac13b66074839f23c79f
Peter Scott <pjscott@iastate.edu>
2011-02-25 17:19:21 -0600

    Ditch the byte packing, and use simple accessor macros for atom serdes.

--- sburb.h
+++ sburb.h
@@ -7,8 +7,28 @@
 #include <stdint.h>
 #include <Judy.h>
 
-/* Serialization/deserialization */
-inline void write_packed_uint32(uint32_t w, uint8_t **ptr);
-inline uint32_t read_packed_uint32(uint8_t **ptr);
+/*********************** Serialization/deserialization ************************/
+
+/* Yarn and offset getters, for atom ids. */
+#define YARN(id)   ((uint32_t)(((id) & (uint64_t)0xFFFFFFFF00000000) >> 32))
+#define OFFSET(id) ((uint32_t)((id) & (uint64_t)0xFFFFFFFF))
+#define PACK_ID(yarn, offset) (((uint64_t)(yarn) << 32) | (uint64_t)(offset))
+
+/* Read an atom from a location. Location pointers are incremented. Pass in only
+   variable names. */
+#define READ_ATOM(id, pred, c, id_ptr, body_ptr) do {  \
+    id = *id_ptr++;                                    \
+    pred = *((uint64_t *)body_ptr);                    \
+    body_ptr += 2;                                     \
+    c = *body_ptr++;                                   \
+  } while (0);
+
+/* Write an atom to a location. Location pointers are incremented. Pass in only
+   variable names. */
+#define WRITE_ATOM(id, pred, c, id_ptr, body_ptr) do { \
+    *id_ptr++ = id;                                    \
+    *(uint64_t *)body_ptr = pred; body_ptr += 2;       \
+    *body_ptr++ = c;                                   \
+  } while (0);
 
 #endif
--- serdes.c
+++ serdes.c
@@ -1,65 +1,37 @@
 #include "sburb.h"
 
-/* Bytes of a 32-bit word: ABCD, where A is most significant. */
-#define BYTE_A(word) (((word) & 0xFF000000) >> 24)
-#define BYTE_B(word) (((word) & 0x00FF0000) >> 16)
-#define BYTE_C(word) (((word) & 0x0000FF00) >> 8)
-#define BYTE_D(word) ((word) & 0xFF)
+/***************************** Atom serialization *****************************/
 
-/* Write a uint32 to ptr, and advance the pointer. */
-inline void write_packed_uint32(uint32_t w, uint8_t **ptr) {
-  uint8_t *p = *ptr;
-  if (w < 254) {                /* Write in one byte */
-    *p = w;
-    *ptr = p+1;
-  } else if (w < 65536) {       /* Use three bytes, big-endian */
-    *p++ = 254;
-    *p++ = BYTE_C(w);
-    *p++ = BYTE_D(w);
-    *ptr = p;
-  } else {                      /* Write five bytes, big-endian */
-    *p++ = 255;
-    *p++ = BYTE_A(w);
-    *p++ = BYTE_B(w);
-    *p++ = BYTE_C(w);
-    *p++ = BYTE_D(w);
-    *ptr = p;
-  }
+/* Atoms are stored in two parallel arrays. The id array stores the ids, and the
+   body array stores (pred, char) pairs. Ids are (yarn, weft) pairs, and
+   chars are 32-bit unsigned ints. Hooray for UTF-32, I guess. */
+
+int main(void) {
+  uint64_t id_a[64];
+  uint32_t body_a[64*3];
+
+  uint64_t id; uint64_t pred; uint32_t c; uint64_t *id_ptr; uint32_t *body_ptr;
+  id_ptr = id_a; body_ptr = body_a;
+  id = PACK_ID(4, 44);
+  pred = PACK_ID(666, 6543210);
+  c = 'Q';
+
+  printf("id:(%u, %u) pred:(%u, %u) '%c'\n", YARN(id), OFFSET(id), YARN(pred), OFFSET(pred), c);
+  WRITE_ATOM(id, pred, c, id_ptr, body_ptr);
+
+  id = PACK_ID(0, 42);
+  pred = PACK_ID(77, 108);
+  c = 'Z';
+
+  printf("id:(%u, %u) pred:(%u, %u) '%c'\n", YARN(id), OFFSET(id), YARN(pred), OFFSET(pred), c);
+  WRITE_ATOM(id, pred, c, id_ptr, body_ptr);
+
+  id = 0; pred = 0; c = '%'; id_ptr = id_a; body_ptr = body_a;
+
+  READ_ATOM(id, pred, c, id_ptr, body_ptr);
+  printf("id:(%u, %u) pred:(%u, %u) '%c'\n", YARN(id), OFFSET(id), YARN(pred), OFFSET(pred), c);
+  READ_ATOM(id, pred, c, id_ptr, body_ptr);
+  printf("id:(%u, %u) pred:(%u, %u) '%c'\n", YARN(id), OFFSET(id), YARN(pred), OFFSET(pred), c);
+
+  return 0;
 }
-
-/* Read a uint32 from ptr, advancing the pointer and returning the int. */
-inline uint32_t read_packed_uint32(uint8_t **ptr) {
-  uint8_t *p = *ptr;
-  uint8_t b = *p++;
-  uint32_t w = 0;
-
-  if (b < 254) {
-    w = b;
-  } else if (b == 254) {
-    w |= *p++;
-    w <<= 8; w |= *p++;
-  } else if (b == 255) {
-    w |= *p++;
-    w <<= 8; w |= *p++;
-    w <<= 8; w |= *p++;
-    w <<= 8; w |= *p++;
-  }
-  
-  *ptr = p; return w;
-}
-
-// int main(void) {
-//   uint8_t arr[128];
-//   uint8_t *ptr = arr;
-// 
-//   write_packed_uint32(42, &ptr);
-//   write_packed_uint32(0, &ptr);
-//   write_packed_uint32(543, &ptr);
-//   write_packed_uint32(99899, &ptr);
-//   ptr = arr;
-//   printf("%u\n", read_packed_uint32(&ptr));
-//   printf("%u\n", read_packed_uint32(&ptr));
-//   printf("%u\n", read_packed_uint32(&ptr));
-//   printf("%u\n", read_packed_uint32(&ptr));
-//   return 0;
-// }
